5f47bf1724196e96670f79509c32192d100a83e2
Vladimir Pavlyukov <yahusim@mail.ru>
2023-02-11 12:56:25 +0300

    полностью выполненное задание

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,31 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    // если после каких-либо манипуляций marks окажется в прототипе,
+    // то оценки все равно будут присваиваться
+    if ('marks' in this) {
+        this.marks.push(...marks);
+    };
 }
 
 Student.prototype.getAverage = function () {
-  
+    if ('marks' in this && this.marks.length >= 0) {
+        return this.marks.reduce((average, mark, _, marks) => average + mark / marks.length, 0);
+    }
+    return 0;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
